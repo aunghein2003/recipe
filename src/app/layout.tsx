b1ba3c1bbfa97afcf23a12e6503370bfe3b6d411
Aung Hein <aungcho2003@gmail.com>
2023-07-12 12:21:56 +0630

    test 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navbar from "../components/navbar";
-import Providers from "../components/providers";
-import Footer from "../components/footer";
+import Navbar from "@/components/Navbar";
+import Providers from "@/components/providers";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
